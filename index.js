0664b7f6ef8175cc3d57a3be89073c087d6b8195
Lindsay <lindsay.chen@yale.edu>
2021-07-25 22:50:06 +0800

    commit tree 676f5f44156d0588e0e1b8e74ca95ec22cc222d4

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solutions in this file
 
-function writeCards(names, thing){
-    let result = []
-    for (let i = 0; i < names.length; i++) {
-        result.push(`Thank you, ${names[i]}, for the wonderful ${thing} gift!`)
-    }
-    return result
-}
+// function writeCards(names, thing){
+//     let result = []
+//     for (let i = 0; i < names.length; i++) {
+//         result.push(`Thank you, ${names[i]}, for the wonderful ${thing} gift!`)
+//     }
+//     return result
+// }
 // function writeCards(names, event) {
 //     let cards = []
 //     for (let i =0; i < names.length; i++) {
@@ -14,6 +14,14 @@ function writeCards(names, thing){
 //     }
 //     return cards
 // }
+function writeCards(names, sth){
+    let result = []
+    for(let i = 0; i < names.length; i++){
+        result.push(`Thank you, ${names[i]}, for the wonderful ${sth} gift!`)
+    }
+    return result
+}
+
 function countDown(num){
     while (num >= 0){
         console.log(num)
